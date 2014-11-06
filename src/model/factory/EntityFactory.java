6f3cf8f7b12aee77fb9de5aaec2340af19605feb
Hesyar Uzuner <hesyar.u@gmail.com>
2014-11-06 20:56:04 +0100

    - implemented EntityFactory - created "Weapon"-Entity - created new object-Resource - created a Test for EntityFactory

--- src/model/factory/EntityFactory.java
+++ src/model/factory/EntityFactory.java
@@ -1,23 +1,116 @@
-
 package model.factory;
 
+import common.Constants;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Entity;
+import model.Material;
 import model.ShipPart;
+import model.entity.Weapon;
+import model.material.Floor;
+import model.material.Wall;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import sun.applet.Main;
 
 /**
  *
  */
-public class EntityFactory extends ShippartFactory{
+public class EntityFactory extends ShippartFactory {
 
-    private EntityFactory(){
-        
-    };
+    private static final String DOM_NODE_OBJECT = "object";
+    private static final String DOM_NODE_NAME = "name";
+    private static final String DOM_ATTR_TYPE = "type";
+    private static final String DOM_NODE_IMAGE = "image";
+    private static final String DOM_NODE_WEIGHT = "weight";
+    private static final String DOM_NODE_VALUE = "value";
+
+    private static EntityFactory instance;
+
+    private static SAXBuilder builder = new SAXBuilder();
+
+    private ArrayList<Entity> entities;
+
+    private EntityFactory() {
+        entities = new ArrayList<Entity>();
+    }
+
+    ;
     
 
     @Override
     public void parse() {
-        
+        File[] materialFolders = new File(Constants.FOLDER_OBJECTS).listFiles();
+        for (int i = 0; i < materialFolders.length; i++) {
+            if (materialFolders[i].isDirectory()) {
+                File[] xmlDefinitions = materialFolders[i].listFiles(new FilenameFilter() {
+
+                    @Override
+                    public boolean accept(File dir, String name) {
+                        return name.toLowerCase().endsWith(Constants.XML_FILE_ENDING);
+                    }
+                });
+                if (xmlDefinitions.length > 0) {
+                    try {
+                        loadObject(xmlDefinitions[0]);
+                    } catch (IOException | JDOMException e) {
+                        // TODO Catch the error properly. Write an error log or something like that.
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
     }
 
+    private void loadObject(File xmlFile) throws IOException, JDOMException {
+
+        Document doc = (Document) builder.build(xmlFile);
+        List<Element> materials = doc.getRootElement().getChildren(DOM_NODE_OBJECT);
+        for (Element element : materials) {
+
+            String type = element.getAttribute(DOM_ATTR_TYPE).getValue(), name = element.getChildText(DOM_NODE_NAME), imagePath = xmlFile.getParent() + File.separator + element.getChildText(DOM_NODE_IMAGE);
+            int value = Integer.parseInt(element.getChildText(DOM_NODE_VALUE)), weight = Integer.parseInt(element.getChildText(DOM_NODE_WEIGHT));
+
+            createInstance(type, imagePath, name, value, weight);
+        }
+    }
+
+    private void createInstance(String type, String imagePath, String name, int value, int weight) throws IOException {
+
+        Entity o = null;
+        switch (type) {
+            default:
+            case "weapon":
+                o = new Weapon();
+                break;
+        }
+
+        o.setName(name);
+        o.setValue(value);
+        o.setWeight(weight);
+
+        entities.add(o);
+    }
+
+    public ArrayList<Entity> getEntities() {
+        if (entities.isEmpty()) {
+            parse();
+        }
+        return entities;
+    }
+
+    public static EntityFactory getInstance() {
+        if (instance == null) {
+            instance = new EntityFactory();
+        }
+        return instance;
+    }
 
     @Override
     public ShipPart instanzise(ShipPart prototype) {
@@ -25,6 +118,5 @@ public class EntityFactory extends ShippartFactory{
         return null;
     }
 
-
-    
+ 
 }
---
+++ test/model/factory/EntityFactoryTest.java
@@ -0,0 +1,46 @@
+package model.factory;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import model.Entity;
+
+import model.Material;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EntityFactoryTest {
+
+    private EntityFactory factory;
+    
+    @Before
+    public void setUp() throws Exception {
+        factory = EntityFactory.getInstance();
+    }
+    
+    @Test
+    public void testInstance()
+    {
+        assertTrue(factory != null);
+    }
+
+    @Test
+    public void testEntities() {
+        try {
+
+            ArrayList<Entity> entities = factory.getEntities();
+
+            assertTrue("Entities have been loaded", !entities.isEmpty());
+
+            Entity object = entities.get(0);
+            assertNotNull(object.getName());
+            System.out.println(object.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Exception was thrown");
+        }
+    }
+
+}
