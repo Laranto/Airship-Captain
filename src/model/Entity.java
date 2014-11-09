27db974fe6a184ef75e0e9b32864f9d0adefce73
Laranto <arni.naurloth@gmail.com>
2014-11-09 17:11:38 +0100

    Added Possibility to rotate an entity and having it larger than 1x1 (is not checked yet)

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -1,6 +1,30 @@
 package model;
 
+import java.awt.geom.Point2D;
+import java.util.Vector;
+
 
 public abstract class Entity extends ShipPart{
+    /**
+     * 2d Vector holding the orientation and dimension of the entity
+     */
+    private Vector<Integer> orientation;
     
+    
+    /**
+     * Position where the Startposition of the entity is placed.
+     */
+    private Point2D position;
+    
+    
+    public void rotateLeft(){
+        Integer tmp = orientation.get(0);
+        orientation.set(0, orientation.get(1));
+        orientation.set(1, tmp*-1);
+    }
+    public void rotateRight(){
+        Integer tmp = orientation.get(0);
+        orientation.set(0, orientation.get(1)*-1);
+        orientation.set(1, tmp);
+    }
 }
