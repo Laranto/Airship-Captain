82403a83f73c5a18c1d9fa4d07697edffaa7ff45
hasher <hesyar_u@hotmail.com>
2014-12-03 20:34:07 +0100

    adapted MoneyTest

--- test/model/economy/MoneyTest.java
+++ test/model/economy/MoneyTest.java
@@ -1,32 +1,47 @@
 package model.economy;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoneyTest {
 
     @SuppressWarnings("deprecation")
-    @Test
-    public void testManipulateMoneyAmount()
+    
+    double startAmount;
+    Money money;
+    
+    @Before
+    public void init()
     {
-
-        double startAmount = 1000;
-        Money money = new Money(startAmount);
+        startAmount = 1000;
+        money = new Money(startAmount);
+    }
+    
+    @Test
+    public void addAmountTest()
+    {
+        money.addAmount(300);
+        Assert.assertTrue(money.getAmount() > startAmount);
+        Assert.assertEquals(1300, money.getAmount(), 0);
         
+        double currentMoneyAmount = money.getAmount();
+        
+        money.addAmount(-300);
+        Assert.assertEquals(currentMoneyAmount, money.getAmount(), 0);
+        
+        
+    }
+    
+    @Test
+    public void removeAmountTest()
+    {
         money.removeAmount(100);
         Assert.assertTrue(money.getAmount() < startAmount);
         Assert.assertEquals(900, money.getAmount(), 0);
         
-        money.addAmount(300);
-        Assert.assertTrue(money.getAmount() > startAmount);
-        Assert.assertEquals(1200, money.getAmount(), 0);
-        
         money.removeAmount(1300);
-        Assert.assertTrue(money.getAmount() > startAmount);
-        Assert.assertEquals(1200, money.getAmount(), 0);
-        
+        Assert.assertEquals(900, money.getAmount(), 0);
     }
-    
-    
     
 }
