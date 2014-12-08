1402cb8e3b5ca862e19dcd816973455072c864da
sutermi <suter.mi@gmail.com>
2014-12-08 21:42:35 +0100

    random scenario

--- src/model/factory/ScenarioFactory.java
+++ src/model/factory/ScenarioFactory.java
@@ -11,8 +11,7 @@ import model.navigation.TradeScenario;
 public class ScenarioFactory {
 
     public static Scenario build() {
-        //int scenarioTyp = new Random().nextInt(3);
-        int scenarioTyp = 1;
+        int scenarioTyp = new Random().nextInt(3);
         switch(scenarioTyp){
             case 0:
                 return new OpportunityScenario("Dies ist ein OpportunityScenario", GameState.getInstance().getAirship().getCaptain());
