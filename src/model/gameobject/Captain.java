809a894c267519fecec9c9d0ef895070f160ddb4
sutermi <suter.mi@gmail.com>
2014-12-08 01:38:42 +0100

    removed captain_hat

--- src/model/gameobject/Captain.java
+++ src/model/gameobject/Captain.java
@@ -3,16 +3,12 @@ package model.gameobject;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.io.Serializable;
 
 import model.GameState;
 import model.economy.Money;
 
 import common.Constants;
-import common.FileUtils;
 
 public class Captain  implements Renderable, Serializable {
     private static final long serialVersionUID = 1L;
@@ -20,28 +16,14 @@ public class Captain  implements Renderable, Serializable {
     private Money money;
     private String name;
     private Point currentPosition;
-    private BufferedImage image;
 
     public Captain() {
         this.money = new Money(Constants.CAPTAIN_START_MONEY);
         this.name = Constants.CAPTAIN_NAME_DEFAULT;
-        try {
-            this.image = FileUtils.loadImage(new File(Constants.CAPTAIN_HAT_IMAGE));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
     
     @Override
     public void render(Graphics2D g) {
-        g.drawImage(
-                image, 
-                (int)currentPosition.getX(),                         /*start x position*/
-                (int)currentPosition.getY(),                         /*start y position*/
-                image.getWidth(),
-                image.getHeight(), 
-                null                                                 /*Image Observer*/
-        );
     }
 
     public Money getMoney() {
